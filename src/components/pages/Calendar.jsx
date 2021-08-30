3a7b0aec7e48a9472e49e8a4117b9255c05134ea
Zdravko Cicin-Sain <zcsain@gmail.com>
2021-08-30 19:47:55 +0200

    Testing access to Google API: getting events, creating events

--- src/components/pages/Calendar.jsx
+++ src/components/pages/Calendar.jsx
@@ -2,6 +2,14 @@ import React from "react";
 
 // Custom
 import RedirectButton from "../dev_elements/RedirectButton";
+import BottomFixedButton from "../elements/BottomFixedButton";
+
+// Material UI
+import AddIcon from "@material-ui/icons/Add";
+
+const handleBottomButtonClick = () => {
+	window.alert("BottomFixedButton from Calendar!");
+};
 
 function Calendar() {
 	return (
@@ -10,6 +18,9 @@ function Calendar() {
 				I am calendar fear me more!!!
 			</p>
 			<RedirectButton url={"/"}>Redirect to Login Page</RedirectButton>
+			<BottomFixedButton icon={<AddIcon />} onClick={handleBottomButtonClick}>
+				Event
+			</BottomFixedButton>
 		</div>
 	);
 }
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-// Custom
+// Custom components
 import LoginCard from "../elements/LoginCard";
+
+// Helper functions
+import dateToISO from "../../helpers/dateToISO";
 
 // Material UI
 import Container from "@material-ui/core/Container";
@@ -20,6 +24,7 @@ const useStyles = makeStyles((theme) => ({
 
 function Login() {
 	const classes = useStyles();
+	const history = useHistory();
 	var gapi = window.gapi;
 
 	// Hide keys/ids in .env so they can be ignored when pushing to GitHub
@@ -35,12 +40,38 @@ function Login() {
 
 	// Authorization scopes required by the API; multiple scopes can be
 	// included, separated by spaces.
-	var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+	var SCOPES =
+		"https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/calendar";
+
+	const date1 = new Date("August 31, 2021 15:00:00");
+	const date2 = new Date("August 31, 2021 16:00:00");
+
+	var event = {
+		summary: "Origin Test Event",
+		start: {
+			dateTime: dateToISO(date1),
+			// dateTime: "2015-05-28T09:00:00-07:00",
+			// timeZone: "America/Los_Angeles",
+		},
+		end: {
+			dateTime: dateToISO(date2),
+			// dateTime: "2015-05-28T17:00:00-07:00",
+			// timeZone: "America/Los_Angeles",
+		},
+		reminders: {
+			useDefault: false,
+			overrides: [
+				{ method: "email", minutes: 24 * 60 },
+				{ method: "popup", minutes: 10 },
+			],
+		},
+	};
 
 	const handleClick = () => {
 		gapi.load("client:auth2", () => {
 			console.log("Loaded client");
 
+			// Initialize Google API client
 			gapi.client.init({
 				apiKey: API_KEY,
 				clientId: CLIENT_ID,
@@ -52,8 +83,54 @@ function Login() {
 				console.log("Loaded calendar v3")
 			);
 
-			gapi.auth2.getAuthInstance().signIn();
+			gapi.auth2
+				.getAuthInstance()
+				.signIn()
+				.then(() => {
+					// Add event to calendar
+					addEvent(event);
+
+					// Get events from calendar
+					// const nowObj = Date.now();
+					// const nowDate = dateToISO(new Date(nowObj));
+					// const weekLaterDate = dateToISO(
+					// 	new Date(nowObj + 7 * 24 * 60 * 60 * 1000)
+					// );
+					// getEvent(nowDate, weekLaterDate);
+				});
 		});
+	};
+
+	const addEvent = (event) => {
+		var request = gapi.client.calendar.events.insert({
+			calendarId: "primary",
+			resource: event,
+		});
+
+		request.execute(function (event) {
+			window.open(event.htmlLink);
+		});
+
+		history.push("/calendar");
+	};
+
+	// Get Events from calendar
+	const getEvent = (startDate, endDate) => {
+		gapi.client.calendar.events
+			.list({
+				calendarId: "primary",
+				// timeMin: new Date().toISOString(),
+				timeMin: startDate,
+				timeMax: endDate,
+				showDeleted: false,
+				singleEvents: true,
+				// maxResults: 10,
+				orderBy: "startTime",
+			})
+			.then((response) => {
+				const events = response.result.items;
+				console.log("EVENTS: ", events);
+			});
 	};
 
 	return (
