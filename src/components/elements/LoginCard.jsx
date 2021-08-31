c133a4ccf83504033af35d850ef8844c88c24738
Zdravko Cicin-Sain <zcsain@gmail.com>
2021-08-31 17:49:26 +0200

    Change LoginCard icon

--- src/components/elements/LoginCard.jsx
+++ src/components/elements/LoginCard.jsx
@@ -8,7 +8,7 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
-import TodayIcon from "@material-ui/icons/Today";
+import EventNoteIcon from "@material-ui/icons/EventNote";
 import Avatar from "@material-ui/core/Avatar";
 
 const useStyles = makeStyles((theme) => ({
@@ -47,7 +47,7 @@ function LoginCard({ title, description, buttonText, onButtonClick }) {
 	return (
 		<Card className={classes.card} raised>
 			<Avatar className={classes.avatar}>
-				<TodayIcon />
+				<EventNoteIcon />
 			</Avatar>
 			<Typography className={classes.title} component="h1" variant="h5">
 				{title}
